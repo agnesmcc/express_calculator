0a49ed54ea8e2a25310fb9bdefeb81fbc80b9413
Agnes <agnes.mccall5@gmail.com>
2024-07-08 21:53:42 -0400

    add route for /mean

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const express = require('express');
 
 const app = express();
 
+app.get('/mean', function(request, response) {
+  let nums = request.query.nums.split(',');
+
+  let mean = 0;
+  for (let i = 0; i < nums.length; i++) {
+    mean += parseInt(nums[i]);
+  }
+  mean = mean / nums.length;
+
+  console.log(mean);
+  return response.json({response: {
+    operation: 'mean',
+    value: mean}});
+});
+
 app.listen(3000, function(){
   console.log('App on port 3000');
 })
