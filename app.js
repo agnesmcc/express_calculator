ec4c9c3ab1da8d842c2213c030ab191de699e13f
Agnes <agnes.mccall5@gmail.com>
2024-07-09 21:44:18 -0400

    add error handling

--- app.js
+++ app.js
@@ -3,37 +3,79 @@ const express = require('express');
 const app = express();
 const {mean, median, mode} = require('./math');
 
-app.get('/mean', function(request, response) {
-  let nums = request.query.nums.split(',');
+function areThereNums(nums) {
+  if (nums.length === 0) {
+    throw new Error('nums is required');
+  }
+}
 
-  let result = mean(nums);
+function areNumsValid(nums) {
+  if (nums.some(isNaN)) {
+    throw new Error('nums must be numbers');
+  }
+}
 
-  console.log(result);
-  return response.json({response: {
-    operation: 'mean',
-    value: result}});
+function parseNums(input) {
+  areThereNums(input);
+  let nums = input.split(',').map(Number);
+  areNumsValid(nums);
+  return nums;
+}
+
+app.get('/mean', function(request, response, next) {
+  try {
+    let nums = parseNums(request.query.nums);
+
+    let result = mean(nums);
+
+    console.log(result);
+    return response.json({response: {
+      operation: 'mean',
+      value: result}});
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.get('/median', function(request, response) {
-  let nums = request.query.nums.split(',');
+app.get('/median', function(request, response, next) {
+  try {
+    let nums = parseNums(request.query.nums);
 
-  let result = median(nums);
+    let result = median(nums);
 
-  console.log(result);
-  return response.json({response: {
-    operation: 'median',
-    value: result}});
+    console.log(result);
+    return response.json({response: {
+      operation: 'median',
+      value: result}});
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.get('/mode', function(request, response) {
-  let nums = request.query.nums.split(',');
+app.get('/mode', function(request, response, next) {
+  try {
+    let nums = parseNums(request.query.nums);
 
-  let result = mode(nums);
+    let result = mode(nums);
 
-  console.log(result);
-  return response.json({response: {
-    operation: 'mode',
-    value: result}});
+    console.log(result);
+    return response.json({response: {
+      operation: 'mode',
+      value: result}});
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use(function(err, req, res, next) {
+  // the default status is 500 Internal Server Error
+  let status = err.status || 500;
+  let message = err.message;
+
+  // set the status and alert the user
+  return res.status(status).json({
+    error: {message, status}
+  });
 });
 
 app.listen(3000, function(){
